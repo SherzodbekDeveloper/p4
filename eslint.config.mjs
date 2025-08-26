f857d337306a33e1c5a653053de579cbd8af92f3
Sherzod <omilxanovs7@gmail.com>
2025-08-26 11:48:38 +0500

    error has been changed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,18 @@
-// eslint.config.mjs
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  js.configs.recommended,
-  {
-    plugins: {
-      react,
-    },
-    rules: {
-      "react/no-unescaped-entities": "off",
-    },
-  },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+export default eslintConfig;
+
+
